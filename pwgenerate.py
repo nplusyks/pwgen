08251259b5049098d3a56e16fce9d7f6301d8dc2
nplusyks <79624219+nplusyks@users.noreply.github.com>
2021-10-21 02:24:33 +0300

    Update pwgenerate.py

--- pwgenerate.py
+++ pwgenerate.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 import string, sys
 from random import *
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+fernet = Fernet(key)
+
 try:
   pwlenght = int(sys.argv[1])
   characters = string.ascii_letters + string.digits
   password =  "".join(choice(characters) for x in range(pwlenght))
-  print(password)
+  encryptedpw = fernet.encrypt(password.encode())
+  print("original string:", password)
+  print("\nencrypted string:", encryptedpw)
+  
 except:
-  print("Usage: ./pwgenerator.py <password lenght>")
+  print("Usage: pwgenerator <password lenght>")
+
 
